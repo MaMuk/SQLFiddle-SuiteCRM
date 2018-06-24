a77234fac82123b5116a5cd4137e8bc4aabbaf79
ChangezKhan <mynameischangezkhan@gmail.com>
2018-06-24 21:36:40 +0530

    formatted files

--- SugarModules/modules/SQLFiddle/index.php
+++ SugarModules/modules/SQLFiddle/index.php
@@ -2,142 +2,18 @@
 
 if(!defined('sugarEntry') || !sugarEntry) die('Not a valid entry point');
 
-global $sugar_config;
-
-switch ($sugar_config['dbconfig']['db_type']) {
-    case 'mysql':
-        $db_type = "text/x-mysql";
-        break;
-
-    case 'mssql':
-        $db_type = "text/x-mssql";
-        break;
-
-    default:
-        $db_type = "text/x-mysql";
-        break;
-}
-
-$comment = null;
+$query = null;
 
 require_once('custom/include/SQLFiddle/get_db_details.php');
 
 set_db_tree();
 
 if($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['code'])) {
-    $comment = $_POST['code'];
+    $query = $_POST['code'];
 }
 
 $sugar_smarty = new Sugar_Smarty();
 
-$sugar_smarty->assign("DBTYPE", $db_type);
-
-$sugar_smarty->assign("QUERY", $comment);
-    
-echo '<link rel="stylesheet" type="text/css" href="custom/include/SQLFiddle/plugin/codemirror/codemirror.css">';
-
-echo '<link rel="stylesheet" type="text/css" href="custom/include/SQLFiddle/plugin/jstree/jstree.min.css">';
-
-echo '<script src="custom/include/SQLFiddle/plugin/jstree/jstree.min.js"></script>';
-
-echo '<link rel="stylesheet" type="text/css" href="custom/include/SQLFiddle/css/custom.css">';
-
-echo '<script src="custom/include/SQLFiddle/plugin/codemirror/codemirror.js"></script>';
-
-echo '<script src="custom/include/SQLFiddle/js/sql.js"></script>';
-
-$editor = "<script>
-           var init = function() {
-                var mime = '$db_type';
-
-                window.editor = CodeMirror.fromTextArea(document.getElementById('code'), {
-                    mode: mime,
-                    indentWithTabs: true, 
-                    smartIndent: true,
-                    lineNumbers: true,
-                    matchBrackets: true,
-                    autofocus: true
-                });
-            }
-            </script>";
-
-$sugar_smarty->assign("EDITOR", $editor);
-
-$jstree = "<script>                                                                        
-           $('#db_tree').jstree({
-           	'core' : {
-		    'data' : {                                                                                              
-		        'url' : 'text.json',
-                        'dataType' : 'json'                                                  
-		    }
- 		}
-           });
-           </script>";
-
-$sugar_smarty->assign("JSTREE", $jstree);
-
-$show_table = "<script>
-                $(document).ready(function(){
-                    $('#preview-form-submit').click(function(){
-                        $.post('index.php?entryPoint=GetDbDetails',
-                        {
-                            data:window.editor.getValue()
-                        },
-			function(response,status){ // Required Callback Function
- 			    var JSONobj = $.parseJSON(response);
-                    	    $('#preview-comment').html('');
-                            if(JSONobj['error']) {
-                                $('#preview-comment').html('<p>'+JSONobj['error']+'</p>');
-                    	    } else {
-                        	if(JSONobj['affected_count'] || JSONobj['affected_count'] == '0') {
-                                    var text = '';
-                            	    if(JSONobj['affected_count'] > '1') {
-                                        text = 's';
-                                    }
-                                    divStructure = '<p>Total '+ JSONobj['affected_count']+' record'+text+' affected.</p><br>';
-                        	} else {
-                            	    var divStructure = '';
-                            	    if(JSONobj['count'].length != '') {
-                            	        divStructure = '<p>Total '+ JSONobj['count']+' records returned.</p><br>';
-                            	    }
-                            	    divStructure += '<table><tbody><thead><tr>'
-                            	    var result = JSONobj['result'];
-                                    var tableHeaders = Object.keys(result[0]);
-
-                            	    var i=0;
-
-                            	    while (tableHeaders.length>i) {
-                                	divStructure += '<th>' + tableHeaders[i] + '</th>';
-                                	i++;
-                            	    }
-
-	                            divStructure += '</tr></thead><tbody>';
-                            
-           	                    for (var key in result) {
-                                	if (result.hasOwnProperty(key)) {
-                                    	    var val = result[key];
-                                    	    divStructure += '<tr>';
-                                    	    for (var subkey in val) {
-                                        	divStructure += '<td>' + val[subkey] + '</td>';
-                                    	    }
-                                    	    divStructure += '</tr>';
-                                	}
-                            	    }
-                            	    divStructure += '</tbody></table>';
-                        	}
-                        	$('#preview-comment').html(divStructure);
-                   	    }			    
-			});
-                    });
-                });
-</script>";
-
-$sugar_smarty->assign('DATATABLE', $show_table);
-
-if(file_exists('custom/modules/SQLFiddle/index.tpl')) {
-    echo $sugar_smarty->display('custom/modules/SQLFiddle/index.tpl');
-} else {
-    echo $sugar_smarty->display('modules/SQLFiddle/index.tpl');
-}
-
-
+$sugar_smarty->assign("QUERY", $query);
+	
+echo $sugar_smarty->display('modules/SQLFiddle/index.tpl');
--- SugarModules/modules/SQLFiddle/index.tpl
+++ SugarModules/modules/SQLFiddle/index.tpl
@@ -1,17 +1,106 @@
-{$EDITOR}
+<link rel="stylesheet" type="text/css" href="custom/include/SQLFiddle/plugin/codemirror/codemirror.css">
+<link rel="stylesheet" type="text/css" href="custom/include/SQLFiddle/plugin/jstree/jstree.min.css">
+<script src="custom/include/SQLFiddle/plugin/jstree/jstree.min.js"></script>
+<link rel="stylesheet" type="text/css" href="custom/include/SQLFiddle/css/custom.css">
+<script src="custom/include/SQLFiddle/plugin/codemirror/codemirror.js"></script>
+<script src="custom/include/SQLFiddle/js/sql.js"></script>
+<body onload="init();">
+	<h1>SQL Fiddle</h1>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-3 leftpane">
+				<div id="db_tree" class="demo">
+				</div>
+			</div>
+			<div class="col-sm-9 rightpane">
+				<div>
+					<form>
+						<textarea class="codemirror-textarea" id="code" name="code">{$QUERY}</textarea>
+						<br>
+						<input type="button" class="btn btn-default" name="preview-form-submit" id="preview-form-submit" value="Submit">
+					</form>
+				</div>
+				<div id="preview-comment">
+					{$QUERY}
+				</div>
+			</div>
+		</div>
+	</div>
+	{literal}
+	<script language="javascript">
+		$('#db_tree').jstree({
+           	'core' : {
+				'data' : {
+					'url' : 'text.json',
+					'dataType' : 'json'
+				}
+			}
+		});
+		var init = function() {
+			var mime = 'text/x-mysql';
 
-<body onload="init();">                                                                                                                                                                                     
-    <h1>SQL Fiddle</h1>                                                                                                                                                                                     
-        <div class="container-fluid">                                                                                                                                                                           
-            <div class="row">                                                                                                                                                                                                   <div class="col-sm-3 leftpane">                                                                                                                                                                                      <div id="db_tree" class="demo"></div>  
-                </div>
-                <div class="col-sm-9 rightpane"><div>
-                <form>                                                                                                                                                                                                              <textarea class="codemirror-textarea" id="code" name="code">{$QUERY}</textarea>
-                    <br>
-                    <input type="button" class="btn btn-default" name="preview-form-submit" id="preview-form-submit" value="Submit">
-                </form>
-           </div>                                                                                                                                                                                              
-           <div id="preview-comment">{$QUERY}</div>                                                                                                                                                      </div>
+			window.editor = CodeMirror.fromTextArea(document.getElementById('code'), {
+				mode: mime,
+				indentWithTabs: true, 
+				smartIndent: true,
+				lineNumbers: true,
+				matchBrackets: true,
+				autofocus: true
+			});
+		}
+		$(document).ready(function(){
+			$('#preview-form-submit').click(function(){
+				$.post('index.php?entryPoint=GetDbDetails',
+				{
+					data:window.editor.getValue()
+				},
+				function(response,status){
+					var JSONobj = $.parseJSON(response);
+					$('#preview-comment').html('');
+					if(JSONobj['error']) {
+						$('#preview-comment').html('<p>'+JSONobj['error']+'</p>');
+					} else {
+						if(JSONobj['affected_count'] || JSONobj['affected_count'] == '0') {
+							var text = '';
+							if(JSONobj['affected_count'] > '1') {
+								text = 's';
+							}
+							divStructure = '<p>Total '+ JSONobj['affected_count']+' record'+text+' affected.</p><br>';
+						} else {
+							var divStructure = '';
+							if(JSONobj['count'].length != '') {
+								divStructure = '<p>Total '+ JSONobj['count']+' records returned.</p><br>';
+							}
+							divStructure += '<table><tbody><thead><tr>'
+							var result = JSONobj['result'];
+							var tableHeaders = Object.keys(result[0]);
+
+							var i=0;
+
+							while (tableHeaders.length>i) {
+								divStructure += '<th>' + tableHeaders[i] + '</th>';
+								i++;
+							}
+
+							divStructure += '</tr></thead><tbody>';
+					
+							for (var key in result) {
+								if (result.hasOwnProperty(key)) {
+									var val = result[key];
+									divStructure += '<tr>';
+									for (var subkey in val) {
+										divStructure += '<td>' + val[subkey] + '</td>';
+									}
+									divStructure += '</tr>';
+								}
+							}
+							divStructure += '</tbody></table>';
+						}
+						$('#preview-comment').html(divStructure);
+					}			    
+				});
+			});
+		});
+	</script>
+	{/literal}
 </body>
-{$JSTREE}
-{$DATATABLE}
